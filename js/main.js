3be51324a373b17655139c14f79ce715f0130775
Ahmed Alhosary <ahmedalhosary67@gmail.com>
2021-11-17 16:57:49 +0200

    new

--- js/main.js
+++ js/main.js
@@ -1,87 +1,86 @@
 // setTimeout(() => {
-// $("#loader").fadeOut("slow"); 
+//   $("#loader").fadeOut("slow");
 
 // }, 2000);
 $(this).on("keydown click", () => {
-    $("#loader").fadeOut("slow");
+  $("#loader").fadeOut("slow");
+  $(".welcome").addClass("animate__animated animate__fadeInDownBig");
+  // $(".cat-table").addClass("animate__animated animate__zoomIn");
+  $("section .export-pdf-button").addClass("animate__animated animate__fadeInLeftBig");
 });
 
 function openModel(type, e) {
-    document.getElementById("modal-wrapper").innerHTML += getModal(type);
-    $("#clothName").focus();
+  document.getElementById("modal-wrapper").innerHTML += getModal(type);
+  $("#clothName").focus();
 }
 
 function closeModal() {
-    document.getElementById("modal-wrapper").innerHTML = "";
+  document.getElementById("modal-wrapper").innerHTML = "";
 }
 
 function addItem(type, event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const clothName = document.getElementById("clothName");
-    const clothColor = document.getElementById("clothColor");
-    const clothType = document.getElementById("clothType");
+  const clothName = document.getElementById("clothName");
+  const clothColor = document.getElementById("clothColor");
+  const clothType = document.getElementById("clothType");
 
-    const targetCategory = document.getElementById(clothType.value);
-    const table = targetCategory.getElementsByClassName(type)[0];
+  const targetCategory = document.getElementById(clothType.value);
+  const table = targetCategory.getElementsByClassName(type)[0];
 
-    clothName.nextSibling.remove();
+  clothName.nextSibling.remove();
 
-    if (!clothName.value == "" || !clothColor.value == "") {
+  if (!clothName.value == "" || !clothColor.value == "") {
+    table.innerHTML += `<span class="task-box">${
+      clothName.value == "" ? "Undefind" : clothName.value
+    } _ ${clothColor.value == "" ? "Undefind" : clothColor.value}</span><br />`;
 
-        table.innerHTML += `<span class="task-box">${
-        clothName.value == "" ? "Undefind" : clothName.value
-      } _ ${clothColor.value == "" ? "Undefind" : clothColor.value}</span><br />`;
+    // if (clothName.value == "") {
+    //   clothName.parentNode.innerHTML += `<span class="text text-danger" style="position: absolute;font-size:20px">please insert cloth name</span>`;
+    //   // return;
+    // }
+    // if (clothColor.value == "") {
+    //   clothColor.parentNode.innerHTML += `<span class="text text-danger" style="position: absolute;font-size:20px">please insert cloth color</span>`;
+    // }
 
-        // if (clothName.value == "") {
-        //   clothName.parentNode.innerHTML += `<span class="text text-danger" style="position: absolute;font-size:20px">please insert cloth name</span>`;
-        //   // return;
-        // }
-        // if (clothColor.value == "") {
-        //   clothColor.parentNode.innerHTML += `<span class="text text-danger" style="position: absolute;font-size:20px">please insert cloth color</span>`;
-        // }
+    // return;
+  }
 
-        // return;
-    }
-
-
-
-
-    document.getElementById("modal-wrapper").innerHTML = "";
+  document.getElementById("modal-wrapper").innerHTML = "";
 }
 
 // Define the function
 // to screenshot the div
 function takeshot() {
-    let div = document.getElementById("table-container");
-    // Use the html2canvas
-    // function to take a screenshot
-    // and append it
-    // to the output div
-    $("#output").children().remove();
-    setTimeout(() => {
-        if ($("#output").parent().parent().parent().hasClass("show")) {
-            html2canvas(div).then(function(canvas) {
-                document.getElementById("output").appendChild(canvas);
-            });
-        }
-    }, 200);
+  let div = document.getElementById("table-container");
+  // Use the html2canvas
+  // function to take a screenshot
+  // and append it
+  // to the output div
+  $("#output").children().remove();
+  setTimeout(() => {
+    if ($("#output").parent().parent().parent().hasClass("show")) {
+      html2canvas(div).then(function (canvas) {
+        document.getElementById("output").appendChild(canvas);
+      });
+    }
+  }, 200);
 }
 
 function downloadPdf() {
-    // var canvas = document.querySelector("canvas");
-    // image = canvas
-    //     .toDataURL("image/png")
-    //     .replace("image/png", "image/octet-stream");
-    // var link = document.createElement("a");
-    // link.download = "my-image.png";
-    // link.href = image;
-    // link.click();
-    $("#organizer").tableHTMLExport({ type: "pdf", filename: "organizer.pdf" });
+  var canvas = document.querySelector("canvas");
+  image = canvas
+    .toDataURL("image/png")
+    .replace("image/png", "image/octet-stream");
+  var link = document.createElement("a");
+  link.download = "my-image.png";
+  link.href = image;
+  link.click();
+  // $("#organizer").tableHTMLExport({ type: "pdf", filename: "organizer.pdf" });
 }
 
 function getModal(type) {
-    return `<div id="custom-model-main" class="custom-model-main model-open">
+  return `<div id="custom-model-main" class="custom-model-main model-open">
 <div class="container">
   <div class="row">
     <div class="col-12 col-md-8">
